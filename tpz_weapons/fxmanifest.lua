0699a472f4df04da5ce73c2d6209b7450b7508c5
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2026-01-09 03:07:58 +0200

    Update fxmanifest.lua

--- tpz_weapons/fxmanifest.lua
+++ tpz_weapons/fxmanifest.lua
@@ -5,7 +5,7 @@ rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aw
 author 'Nosmakos'
 description 'TPZ-CORE Weapons'
 
-version '1.0.7'
+version '1.0.8'
 
 ui_page 'html/index.html'
 
@@ -22,6 +22,7 @@ dependencies {
 }
 
 lua54 'yes'
+
 
 
 
